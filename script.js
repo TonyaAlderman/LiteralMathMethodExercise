91fc98c88f963be85ed58c57240872209c31a61c
TonyaAlderman <talderm99@gmail.com>
2021-09-23 08:06:43 -0500

    finished LiteralMathMethod

---
+++ script.js
@@ -0,0 +1,16 @@
+let warmHugs="Hi, I\'m Olaf and I like warm hugs.";
+console.log(warmHugs.toUpperCase());
+console.log(warmHugs.replace("like", "love"));
+warmHugs="Hi, I\'m Olaf and I love warm hugs."
+beenImpaled="Oh, look at that. I've been impaled."
+console.log(beenImpaled.slice(18, 36));
+const dotDotDot="..."
+let skullBones=`I don\'t have a skull${dotDotDot}or bones.`;
+console.log(skullBones);
+const math1=Math.PI
+console.log(math1);
+let randomNumber = Math.random();
+randomNumber *= 3;
+console.log(randomNumber);
+
+
